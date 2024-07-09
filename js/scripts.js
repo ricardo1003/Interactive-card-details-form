0636a68b4d92dc366f5d98ca020fc9707e7a52cd
unknown <ricardodonado1105@gmail.com>
2024-07-09 15:53:06 -0500

    the number of the card is the one in the input

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 const cvcNumberElement = document.getElementsByClassName("cvc")[0]
 const cardNumberElement = document.getElementsByClassName("number")[0]
+const cardNumberGroup = [...document.getElementsByClassName("numberGroup")]
 const cardNameElement = document.getElementsByClassName("name")[0]
 const cardDateElement = document.getElementsByClassName("cardDate")
 
@@ -28,3 +29,14 @@ for(let i=0; i<inputElements.length; i++){
         inputElements[i].classList.remove("invalid")
     })
 }
+
+inputElements[1].addEventListener("input",()=>{
+    let group1 = inputElements[1].value.trim().slice(0,4)
+    cardNumberGroup[0].innerHTML = group1
+    let group2 = inputElements[1].value.trim().slice(4,8)
+    cardNumberGroup[1].innerHTML = group2
+    let group3 = inputElements[1].value.trim().slice(8,12)
+    cardNumberGroup[2].innerHTML = group3
+    let group4 = inputElements[1].value.trim().slice(12,16)
+    cardNumberGroup[3].innerHTML = group4
+})
