dce444c669b52a8c5ff70ca8d5582613d6b41478
unknown <ricardodonado1105@gmail.com>
2024-07-09 14:58:41 -0500

    getting all the elements as constants in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-console.log("hello world!")
+const cvcNumberElement = document.getElementsByClassName("cvc")[0]
+const cardNumberElement = document.getElementsByClassName("number")[0]
+const cardNameElement = document.getElementsByClassName("name")[0]
+const cardDateElement = document.getElementsByClassName("cardDate")
+
+const sucessState = document.getElementsByClassName("complete")[0]
+const formElement = document.getElementsByTagName("form")[0]
+
+const submitButtonElement = document.getElementsByTagName("button")[0]
+const continueButtonElement = document.getElementsByTagName("button")[1]
+
+const invalidParagraphs = document.getElementsByClassName("invalidParagraph")
+const inputElements = document.getElementsByTagName("input")
