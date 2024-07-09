b107493d5ce7d9ea0e751a342049ff80a42bbea7
unknown <ricardodonado1105@gmail.com>
2024-07-09 15:23:13 -0500

    empty validation done

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,22 @@ const formElement = document.getElementsByTagName("form")[0]
 const submitButtonElement = document.getElementsByTagName("button")[0]
 const continueButtonElement = document.getElementsByTagName("button")[1]
 
-const invalidParagraphs = document.getElementsByClassName("invalidParagraph")
-const inputElements = document.getElementsByTagName("input")
+const invalidParagraphs = [...document.getElementsByClassName("invalidParagraph")]
+const inputElements = [...document.getElementsByTagName("input")]
+
+
+submitButtonElement.addEventListener("click",()=>{
+    for(let i=0; i<inputElements.length; i++){
+        if(inputElements[i].value.trim() === ""){
+            invalidParagraphs[i].classList.remove("noDisplay")
+            inputElements[i].classList.add("invalid")
+        }
+    }
+})
+
+for(let i=0; i<inputElements.length; i++){
+    inputElements[i].addEventListener("input",()=>{
+        invalidParagraphs[i].classList.add("noDisplay")
+        inputElements[i].classList.remove("invalid")
+    })
+}
