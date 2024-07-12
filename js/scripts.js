fe9a658de387ccecb47e5ba6a10f1f3bcaa26b13
unknown <ricardodonado1105@gmail.com>
2024-07-11 23:48:48 -0500

    small adjustment

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ const cvcNumberElement = document.getElementsByClassName("cvc")[0]
 const cardNumberElement = document.getElementsByClassName("number")[0]
 const cardNumberGroup = [...document.getElementsByClassName("numberGroup")]
 const cardNameElement = document.getElementsByClassName("name")[0]
-const cardDateElement = document.getElementsByClassName("cardDate")
+const cardDateElement = document.getElementsByClassName("cardDate")[0]
 
 const sucessState = document.getElementsByClassName("complete")[0]
 const formElement = document.getElementsByTagName("form")[0]
@@ -13,18 +13,29 @@ const continueButtonElement = document.getElementsByTagName("button")[1]
 const labelElements = document.getElementsByTagName("label")
 const invalidParagraphs = [...document.getElementsByClassName("invalidParagraph")]
 const inputElements = [...document.getElementsByTagName("input")]
+const dateInputs = document.getElementsByClassName("dateInputs")
 
+
+let validation1 = false
+let validation2 = false
+let validation3 = false
 
 submitButtonElement.addEventListener("click",()=>{
     for(let i=0; i<inputElements.length; i++){
         if(inputElements[i].value.trim() === ""){
             invalidParagraphs[i].classList.remove("noDisplay")
             inputElements[i].classList.add("invalid")
+        }else{validation1 = true}
+        if(inputElements[1].value.trim().search(/[^\d\s]/g) !== -1){
+            invalidParagraphs[1].classList.remove("noDisplay")
+            inputElements[1].classList.add("invalid")
+        }else{
+            validation2 = true
         }
     }
-    if(inputElements[1].value.trim().search(/\D+/g) !== -1){
-        invalidParagraphs[1].classList.remove("noDisplay")
-        inputElements[1].classList.add("invalid")
+    if(validation1 && validation2 && validation3){
+        sucessState.classList.toggle("noDisplay")
+        formElement.classList.toggle("noDisplay")
     }
 })
 
@@ -34,13 +45,17 @@ for(let i=0; i<inputElements.length; i++){
         inputElements[i].classList.remove("invalid")
     })
 }
-
+inputElements[0].addEventListener("input",()=>{
+    cardNameElement.innerHTML = inputElements[0].value.trim()
+})
 inputElements[1].addEventListener("input",(event)=>{
     const noSpace = event.target.value.replace(/\s/g, "")
     let formattedInput = ""
     for (let i=0; i< noSpace.length;i++){
         if(i>0 && i % 4 === 0){
             formattedInput += " "
+        }else{
+            validation3 = true
         }
         formattedInput += noSpace[i]
     }
@@ -75,12 +90,49 @@ inputElements[1].addEventListener("input",(event)=>{
         }
     }
 })
+for(let i=0; i<dateInputs.length; i++){
+    dateInputs[i].addEventListener("input",()=>{
+        dateInputs[i].value = Math.abs(dateInputs[i].value)
+        if(dateInputs[i].value.length > 2){
+            const splitted = dateInputs[i].value.split("")
+            splitted[2] = ""
+            dateInputs[i].value = splitted.join("")
+        }
+        cardDateElement.innerHTML = `${Math.abs(dateInputs[0].value)}/${Math.abs(dateInputs[1].value)}`
+        if(cardDateElement.innerHTML.search(/\//g) === 1){
+            const splitted = cardDateElement.innerHTML.split("")
+            splitted.splice(1, 0, "0")
+            cardDateElement.innerHTML = splitted.join("")
+        }
+        if (cardDateElement.innerHTML.length === 3){
+            const splitted = cardDateElement.innerHTML.split("")
+            splitted.push("00")
+            cardDateElement.innerHTML = splitted.join("")
+        }
+        if (cardDateElement.innerHTML.length === 4){
+            const splitted = cardDateElement.innerHTML.split("")
+            splitted.push("0")
+            cardDateElement.innerHTML = splitted.join("")
+        }
+    })
+}
+inputElements[4].addEventListener("input",()=>{
+    if(inputElements[4].value.length > 3){
+        const splitted = inputElements[4].value.split("")
+        splitted[2] = ""
+        inputElements[4].value = splitted.join("")
+    }else{
+        cvcNumberElement.innerHTML = inputElements[4].value
+    }
+})
 
 for(let i=0; i<inputElements.length; i++){
     inputElements[i].addEventListener("focus",()=>{
-        labelElements[i].classList.add("focusedLabel")
+        if(inputElements[i].classList.contains("invalid") === false){
+            labelElements[i].classList.add("focusedLabel")
+        }
     })
     inputElements[i].addEventListener("blur",()=>{
-        labelElements[i].classList.remove("focusedLabel")
+            labelElements[i].classList.remove("focusedLabel")
     })
 }
